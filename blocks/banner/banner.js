c1e234695ad4a834f79aec19a0e4edb121b1e0ae
roja thimmappa <roja.thimmappa@infosys.com>
2026-08-30 12:21:21 +0530

    Enhanced banner block

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,19 +1,21 @@
 export default function decorate(block) {
-  const rows = [...block.querySelectorAll(':scope > div')];
+  const rows = [...block.children];
 
   const imageUrl = rows[1]?.textContent.trim();
   const title = rows[2]?.textContent.trim();
   const description = rows[3]?.textContent.trim();
 
-  const ctaData = rows[4]?.textContent.trim();
-  const [ctaText, ctaUrl] = ctaData.split('|');
+  const [ctaText, ctaUrl] =
+    rows[4]?.textContent.split('|').map((item) => item.trim());
 
   block.innerHTML = `
     <div class="banner-content">
-      ${imageUrl}
+      <img src=${imageUrl}/>
       <h2>${title}</h2>
       <p>${description}</p>
-      ${ctaUrl}${ctaText}</a>
+      <a href=${ctaUrl}>
+        ${ctaText}
+      </a>
     </div>
   `;
 }
